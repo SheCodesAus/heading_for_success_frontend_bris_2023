c0c841e57a530431e8d097a79e069dc62689119f
mmtovarm <monicamtovar@yahoo.com>
2023-10-09 19:09:16 +1000

    initial homepage and navbar for commmit

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+//import App from './App.jsx'
+//import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import NavBar from './components/NavBar';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <NavBar/>,
+    children: [
+      { path: '/', element: <HomePage /> },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <AuthProvider> */}
+    {/* Wrap app in router provider to render  */}
+      <RouterProvider router={router} />
+    {/* </AuthProvider> */}
   </React.StrictMode>,
 )
---
+++ app/src/pages/HomePage/index.jsx
@@ -0,0 +1,8 @@
+import "./styles.css";
+
+const HomePage = () => {
+    return (<h1> My homepage</h1>);
+}
+
+
+export default HomePage;
