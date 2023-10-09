53a1798fcd50f1a04f0651a18067d64b203af049
mmtovarm <monicamtovar@yahoo.com>
2023-10-09 20:25:11 +1000

    adding app folder with beginning of frontend code

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -2,25 +2,33 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 //import App from './App.jsx'
 //import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import HomePage from './pages/HomePage';
+//import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './Pages/Homepage';
 import NavBar from './components/NavBar';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <NavBar/>,
-    children: [
-      { path: '/', element: <HomePage /> },
-    ],
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <AuthProvider> */}
-    {/* Wrap app in router provider to render  */}
-      <RouterProvider router={router} />
-    {/* </AuthProvider> */}
+    <HomePage />
   </React.StrictMode>,
 )
+
+
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <NavBar/>,
+//     children: [
+//       { path: '/', element: <HomePage /> },
+//     ],
+//   },
+// ]);
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+  //<React.StrictMode>
+    {/* <AuthProvider> */}
+    {/* Wrap app in router provider to render  */}
+     // <RouterProvider router={router} />
+    {/* </AuthProvider> */}
+ // </React.StrictMode>,
+//)
