c7e061d9e2fc4cce1c553d8d43316a6f0136094f
mmtovarm <monicamtovar@yahoo.com>
2023-10-10 19:35:16 +1000

    remove fake comment

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-//this is a fake change
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
