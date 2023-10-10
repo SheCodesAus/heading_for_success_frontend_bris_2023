0ce41a5b294c147965417dbae18223f8472166ae
SKrpio <134825964+SKrpio@users.noreply.github.com>
2023-10-10 19:31:11 +1000

    adminHome and logos added

---
+++ app/src/AdminHome/AdminHome.jsx
@@ -0,0 +1,8 @@
+function AdminHome() {
+    return (
+        <div id="admin-home">
+            
+        </div>
+    );
+}
+export default AdminHome;
---
+++ app/src/ApplicantHome/ApplicantHome.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Goldlogo from /images/GoldLogo.png
+
+function ApplicantHome() {
+    return (
+        <div className="apply-instructions">
+            <img src={Goldlogo} alt="Cupcake Logo" />
+            <h1>How To Apply</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+    );
+}
+
+export default ApplicantHome
