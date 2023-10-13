41acf772f8e80eda5e122babce9367a19f4ee3ff
cinzialoi <cinzia.loi@hotmail.com>
2023-10-13 18:24:46 +1000

    Reset the application pulled from Main, now it works as per ReactJS requirements. New baseline to start from

--- app/src/pages/HomePage/HomePage.jsx
+++ app/src/pages/HomePage/HomePage.jsx
@@ -2,21 +2,14 @@ import "./HomePage.css";
 import HeroLogo from '../../components/HeroLogo/HeroLogo';
 //react functions go here
 
-const HomePage = () => {
-
-
+function HomePage() {
     return (
-
         <div className="homepage-container">
             <h1> My homepage</h1>
-
             <p>this is my homepage</p>
             <HeroLogo />
             {/* {console.log("hello world")} */}
-                
-
         </div>
-
     );
 }
 
---
+++ app/src/pages/ProgramOpenPage.jsx
@@ -0,0 +1,6 @@
+function ProgramOpenPage() {
+    
+    return<h1>This is the Program page visible to students.</h1>;
+}
+
+export default ProgramOpenPage
