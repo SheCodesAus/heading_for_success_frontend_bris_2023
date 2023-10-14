9ed16b0e86adec0a2c77205601fb0d04b357a86c
cinzialoi <cinzia.loi@hotmail.com>
2023-10-14 17:37:00 +1000

    Created base form to add a scholarship against a program. Deleted the Create Scholarship page as realized it is not needed. Imported the scholarship component into ProgramDetails page because this is where we will be displaying it and using it. Added some dummy text to that page also. Edited the Main.jsx to reflect these changes

--- src/pages/Admin/ProgramDetails.jsx
+++ src/pages/Admin/ProgramDetails.jsx
@@ -1,5 +1,15 @@
+import ScholarshipForm from "../../components/NewScholarship/ScholarshipForm";
+
 function ProgramDetails() {
-    return<h1>This is the project page.</h1>;
+    return(
+        <>
+        <h1>This is the project page.</h1>
+        <p>Here we will see the program details, the related scholarships, the related applicants</p>
+        <p>and we will be able to add a new scholarship to the program via the below component.</p>
+        <h2>This is he scholarship component below</h2>
+        <ScholarshipForm />
+        </>
+    );
 }
 
 export default ProgramDetails
