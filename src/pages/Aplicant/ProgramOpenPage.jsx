ad69a471b72057b1f7f82dff9c6b46b9507c0909
Julie E Powell <JEPowell@proton.me>
2023-10-17 09:27:47 +1000

    found and fixed a spelling error

--- src/pages/Aplicant/ProgramOpenPage.jsx
+++ src/pages/Aplicant/ProgramOpenPage.jsx
@@ -1,5 +1,5 @@
 import { useOpenProgram } from "../../hooks/use-program";
-import OpenPrograms from "../../components/OpenPrograms/OpenProgams";
+import OpenPrograms from "../../components/OpenPrograms/OpenPrograms";
 
 function ProgramOpenPage() {
     const { programs, isLoading, error } = useOpenProgram();
