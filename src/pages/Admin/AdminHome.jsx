204cf90572b3d840bd2a331f8c490750ff3937d5
SKrpio <134825964+SKrpio@users.noreply.github.com>
2023-10-19 13:32:51 +1000

    adminhome updates, added two new icon images

--- src/pages/Admin/AdminHome.jsx
+++ src/pages/Admin/AdminHome.jsx
@@ -2,30 +2,28 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import ScholarshipsIcon from '../../Images/Scholarships-Icon.svg'
 import ApplicantsIcon from '../../Images/Applicants-Icon.svg'
+import ProgramsIcon from '../../Images/Programs-Icon.svg'
+import CreateAdminIcon from '../../Images/CreateAdmin-Icon.svg'
 
 function AdminHome() {
     return (
         <>
             <div id="admin-home">
             <div className="programs">
-            <Link to="/program">
-                <button type="submit">
-                    Programs  
-                        </button>
+            <Link to="/programs">
+            <img src={ProgramsIcon} width="150" height="150" />
                     </Link>
                 </div>
 
                 <div className="applicants">                    
-                    <Link to="/Applications">
+                    <Link to="/program/:id/application">
                         <img src={ApplicantsIcon} width="150" height="150" />
                     </Link>
                 </div>
 
                 <div className="createAdmin">
-                    <Link to="/CreateUser">
-                        <button type="submit">
-                    Create Admin
-                </button>
+                    <Link to="/user">
+                    <img src={CreateAdminIcon} width="150" height="150" />
                 </Link>
                 </div>
 
