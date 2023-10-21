57f1e8be17ea62e5f9f53684285df27707bf7591
Tracey Nguyen <trace_n@hotmail.com>
2023-10-21 12:27:13 +1000

    api is completed for create a new program

---
+++ src/components/NewProgramForm/NewProgramForm.jsx
@@ -0,0 +1,182 @@
+import { useState } from 'react';
+import PostProgram, { postProgram } from '../../api/post-program';
+import MessageCard from '../MessageCard/MessageCard';
+
+
+function NewProgramForm() {
+    const [isLoading, setIsLoading] = useState(false);
+    const [messageBlock, setMessageBlock] = useState(false);
+    const [programData, setprogramData] = useState({
+        program_name: '',
+        location: '',
+        intake: '',
+        description: '',
+        image: '',
+        date_start: '',
+        date_end: '',
+        application_date_start: '',
+        application_date_end: ''
+    })
+
+    const handleChange = (e) => {
+        setprogramData({
+            ...programData,
+            [e.target.id]: e.target.value
+        })
+     
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setIsLoading(true)
+
+        if (programData.program_name) {
+            console.log(programData);
+            postProgram(
+                programData.program_name,
+                programData.location,
+                programData.intake,
+                programData.description,
+                programData.image,
+                programData.date_start,    
+                programData.date_end,
+                programData.application_date_start,
+                programData.application_date_end,            
+            ).then((response) => {
+                setMessageBlock(true);                
+            })
+        }
+    }
+
+    const location_options = [
+        {
+            label: 'Brisbane',
+            value: 'Brisbane',
+        },
+        {
+            label: 'Sydney',
+            value: 'Sydney',
+        },
+        {
+            label: 'Melbourne',
+            value: 'Melbourne',
+        },
+        {
+            label: 'Perth',
+            value: 'Perth',
+        },
+        {
+            label: 'Canberra',
+            value: 'Canberra',
+        },
+        {
+            label: 'Darwin',
+            value: 'Darwin',
+        },
+        {
+            label: 'Tasmania',
+            value: 'Tasmania',
+        },
+    ];
+    return (
+        <form className='AppForm'>
+            <div>
+                <label htmlFor='program_name'>Program Name</label>
+                <input
+                    type='text'
+                    id='program_name'
+                    placeholder='Enter name of program'
+                    onChange={handleChange}
+                />
+            </div>
+            <div>
+                <label htmlFor='location'>Location</label>
+                <select
+                    name='location'
+                    id='location'
+                    onChange={handleChange}>
+                    {location_options.map((location_options,key) => (
+                        <option 
+                            key={key}
+                            value={location_options.value}
+                        >
+                            {location_options.label}
+                        </option>
+                    ))}
+                </select>                    
+            </div>
+            <div>
+                <label htmlFor='intake'>Intake</label>
+                <input
+                    type='text'
+                    id='intake'
+                    placeholder='Month/ Year'
+                    onChange={handleChange}
+                />
+            </div>
+            <div>
+                <label htmlFor='description'>Details</label>
+                <textarea
+                    type='text-area'
+                    id='description'
+                    placeholder='Description of Program'
+                    onChange={handleChange}
+                    rows='5'
+                    cols='30'                     
+                />
+            </div>
+            <div>
+                <label htmlFor='image'>Image link</label>
+                <input
+                    type='text'
+                    id='image'
+                    onChange={handleChange}
+                />
+            </div>
+            <div>
+                <label htmlFor='date_start'>Start Date</label>
+                <input
+                    type='date'
+                    id='date_start'
+                    placeholder='Start date?'
+                    onChange={handleChange}
+                />
+            </div>
+            <div>
+                <label htmlFor='date_end'>End Date</label>
+                <input
+                    type='date'
+                    id='date_end'
+                    placeholder='End date?'
+                    onChange={handleChange}
+                />
+            </div>
+            <div>
+                <label htmlFor='application_date_start'> Date Applications Start </label>
+                <input
+                    type='date'
+                    id='application_date_start'
+                    placeholder='When Applications Start'
+                    onChange={handleChange}
+                />
+            </div>
+            <div>
+                <label htmlFor='application_date_end'> Date Applications End</label>
+                <input
+                    type='date'
+                    id='application_date_end'
+                    placeholder='When Applications End'
+                    onChange={handleChange}
+                />
+            </div>
+            <button type='submit' onClick={handleSubmit}>Create </button>
+            {messageBlock && 
+                <MessageCard 
+                    message='Program created successfully' 
+                />
+            }
+        </form>
+    )
+}
+
+export default NewProgramForm
