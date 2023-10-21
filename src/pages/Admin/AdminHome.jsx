69df38cd573b6f96ec1cdf5198b92684441e5182
SKrpio <134825964+SKrpio@users.noreply.github.com>
2023-10-21 10:11:29 +1000

    adminhome styling - wip

--- src/pages/Admin/AdminHome.jsx
+++ src/pages/Admin/AdminHome.jsx
@@ -1,33 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import './AdminHome.css'
+import homeCartoon from '../../Images/HomePage-1-Cartoon.svg'
+import './AdminHome.css'
 
 function AdminHome() {
     return (
-        <>
-        <div id="admin-home">
-            <Link to="/program">
-                <button type="submit">
-                    Programs
-                </button>
-            </Link>
-            <Link to="/applicant">
-                <button type="submit">
-                    Applications
-                </button>
-            </Link>
-            <Link to="/user">
-                <button type="submit">
-                    Create Admin
-                </button>
-            </Link>
-            <Link to="/scholarship">
-                <button type="submit">
-                    Scholarships
-                </button>
-            </Link>
-        </div>
-        </>
+        <div>
+            <h1>Welcome, Administrator</h1>
+        <img src={homeCartoon} class="homeCartoon"></img>
+            </div>
     );
 }
 export default AdminHome;
