0397aadbcdb01a566460d9f622b7d33c197cde80
SKrpio <134825964+SKrpio@users.noreply.github.com>
2023-10-22 14:57:37 +1000

    attempted styling for programslist on homepage, WIP

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,25 +1,20 @@
 // import "../../main.css"
 import "./HomePage.css";
-import HeroLogo from '../../components/HeroLogo/HeroLogo';
 import UpdatedCartoon from '../../Images/UpdatedCartoon.svg'
-import { Link } from "react-router-dom";
 import Header from '../../components/Header/header.jsx';
 import Footer from '../../components/Footer/Footer.jsx';
-// import OpenPrograms from "../../components/OpenPrograms/OpenProgams";
+import OpenProgramsList from "../../components/OpenProgramsList/OpenProgamsList";
 //react functions go here
 
 function HomePage() {
     return (
         <div className="homepage-container">
             <Header />
-            {/* <OpenPrograms /> */}
-            {/* <h1> My homepage</h1> */}
-            {/* <div class="mainThings">
-                <HeroLogo /> 
-                <Link to='/programOpen'>
-                    <div class="applyBox"><p>Browse open programs</p></div>
-                </Link>
-            </div> */}
+            <div class="programs">
+                <div class="column">
+                <OpenProgramsList />
+                </div>
+                </div>
             <img src={UpdatedCartoon} class="homeCartoon"></img>
             <Footer/>
         </div>
