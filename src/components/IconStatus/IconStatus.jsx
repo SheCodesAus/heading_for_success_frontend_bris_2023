0811155acea096edb85dedc51821c6ba745164e1
Tracey Nguyen <trace_n@hotmail.com>
2023-10-22 11:56:21 +1000

    completed All Programs List Admin page including styling

---
+++ src/components/IconStatus/IconStatus.jsx
@@ -0,0 +1,31 @@
+import './IconStatus.css';
+
+const IconStatus = (props) => {
+
+    return (
+        <div> 
+        { props.status === 'Closed' && 
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="program-icons-close" title="Closed">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+          </svg>
+        }
+
+        { props.status === 'Open' &&
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="program-icons-open" title="Open">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+        </svg>
+      
+        }
+        
+        { props.status === 'Future' &&
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="program-icons-future" title="Future">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+        </svg>
+      
+        }
+
+        </div>
+    );
+}
+
+export default IconStatus;
--- src/pages/Admin/Programs.jsx
+++ src/pages/Admin/Programs.jsx
@@ -1,17 +1,25 @@
 import { usePrograms } from "../../hooks/use-programs";
 import { useAuth } from "../../hooks/use-auth";
+import { useState } from "react";
 import LoginForm from "../../components/AdminLogin/LoginForm";
 import ProgramCard from "../../components/ProgramCard/ProgramCard";
-// import Spinner from "../../components/Spinner/Spinner";
+import Spinner from "../../components/Spinner/Spinner";
 
 function Programs() {
 
     const {auth, setAuth} = useAuth();
     const { allPrograms, isLoading, error, setAllPrograms } = usePrograms();
+    const [sortToggleAsc, setSortToggleAsc] = useState(true);
+    const [sortToggleDesc, setSortToggleDesc] = useState(false);
+    const [sortToggleAscLoc, setSortToggleAscLoc] = useState(true);
+    const [sortToggleDescLoc, setSortToggleDescLoc] = useState(false);
+    const [sortToggleAscProgStat, setSortToggleAscProgStat] = useState(true);
+    const [sortToggleDescProgStat, setSortToggleDescProgStat] = useState(false);
+    const [sortToggleAscAppStatus, setSortToggleAscAppStatus] = useState(true);
+    const [sortToggleDescAppStatus, setSortToggleDescAppStatus] = useState(false);
 
     if (isLoading) {
-        // return (<Spinner />)
-        return <p>Is Loading...</p>
+        return (<Spinner />)
     }
 
     if (error) {
@@ -32,25 +40,89 @@ function Programs() {
 
     const handleClick = (event) => {
         const text = event.target.innerText;
-        // console.log("evt click",event, event.target, event.target.id, event.target.innerText);
-        if (text === 'Program') {
-            // const sortedProgramDetail = [...allPrograms].sort((a, b) => {
-            //     return a[program_name].localeCompare(b[program_name]);
-            // }); 
-            // const sortedProgramDetail = [...allPrograms].sort((a,b) => {
-            //     a.program_name.localeCompare(b.program_name)
-            // });
-            const sortedProgramDetail = allPrograms.sort((a, b) => {
-                return a.program_name.localeCompare(b.program_name);
-            }); 
+        console.log("evt click",event, event.target, event.target.id, event.target.innerText);
+        if (text === 'Program' || event.target.id === 'program') {
+            console.log(sortToggleAsc);
             
+            const sortedProgramDetail = [...allPrograms].sort((a, b) => {
+                if (sortToggleAsc) {
+                    return a.program_name.localeCompare(b.program_name);
+                } else {
+                    return b.program_name.localeCompare(a.program_name);
+                }
+            }); 
+            if (sortToggleAsc) {
+                setSortToggleDesc(true);
+                setSortToggleAsc(false);  
+
+            } else {
+                setSortToggleDesc(false);  
+                setSortToggleAsc(true); 
+            }
             setAllPrograms(sortedProgramDetail);
-            // console.log(sortedProgramDetail);
-            console.log(allPrograms);
         }
 
-        if (text === 'Location') {
+        if (text === 'Location' || event.target.id === 'location') {
+            console.log(sortToggleAscLoc);
+            
+            const sortedProgramDetail = [...allPrograms].sort((a, b) => {
+                if (sortToggleAscLoc) {
+                    return a.location.localeCompare(b.location);
+                } else {
+                    return b.location.localeCompare(a.location);
+                }
+            }); 
+            if (sortToggleAscLoc) {
+                setSortToggleDescLoc(true);
+                setSortToggleAscLoc(false);  
 
+            } else {
+                setSortToggleDescLoc(false);  
+                setSortToggleAscLoc(true); 
+            }
+            setAllPrograms(sortedProgramDetail);
+        }
+
+        if (text === 'Program Status' || event.target.id === 'program-status') {
+            console.log(sortToggleAscProgStat);
+            
+            const sortedProgramDetail = [...allPrograms].sort((a, b) => {
+                if (sortToggleAscProgStat) {
+                    return a.date_start.localeCompare(b.date_start);
+                } else {
+                    return b.date_start.localeCompare(a.date_start);
+                }
+            }); 
+            if (sortToggleAscProgStat) {
+                setSortToggleDescProgStat(true);
+                setSortToggleAscProgStat(false);  
+
+            } else {
+                setSortToggleDescProgStat(false);  
+                setSortToggleAscProgStat(true); 
+            }
+            setAllPrograms(sortedProgramDetail);
+        }
+
+        if (text === 'Application Status' || event.target.id === 'application-status') {
+            console.log(sortToggleAscAppStatus);
+            
+            const sortedProgramDetail = [...allPrograms].sort((a, b) => {
+                if (sortToggleAscAppStatus) {
+                    return a.application_date_start.localeCompare(b.application_date_start);
+                } else {
+                    return b.application_date_start.localeCompare(a.application_date_start);
+                }
+            }); 
+            if (sortToggleAscAppStatus) {
+                setSortToggleDescAppStatus(true);
+                setSortToggleAscAppStatus(false);  
+
+            } else {
+                setSortToggleDescAppStatus(false);  
+                setSortToggleAscAppStatus(true); 
+            }
+            setAllPrograms(sortedProgramDetail);
         }
     } 
 
@@ -59,32 +131,131 @@ function Programs() {
         
         <>
         
-            <h1>Admin View - All programs,  open and closed</h1>
+            <h1>All Programs List</h1>
+            
+            <div className="program-legend"> Icon Legend
+                <div className="program-legend-icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="program-icons-close" title="Closed">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    
+                    </svg> Closed
+                </div>
+                <div className="program-legend-icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="program-icons-open" title="Open">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg> Open
+                    </div>
+                <div className="program-legend-icon">      
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="program-icons-future" title="Future">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg> Future
+                </div>
+            </div>
             
             { auth.token ? (
                 <>  
                 { ( allPrograms.length > 0 ) && 
-                // <p>{allPrograms[0].program_name}</p>
                 <>
-                    <ul>
+                    <ul className="program-card-list">
                     <li>
                         <div className='program-card program-card-header'> 
-                        <div className='program-card-grid' id='program' value='program' onClick={handleClick}>
-                            <h3>Program</h3>
+                        <div 
+                            className='program-card-grid' 
+                            id='program'         
+                            value='program' 
+                            // onClick={handleClick}
+                        > 
+                            <button onClick={handleClick} id='program' value='program' className='program-card-sort-btn'>
+                                <div className='program-card-sort'>
+                                    <h3>Program 
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" 
+                                        className={sortToggleAsc ? 
+                                        'program-icons-sort' : 'display-none'} id='program'>
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 5.25l-7.5 7.5-7.5-7.5m15 6l-7.5 7.5-7.5-7.5" />
+                                        </svg>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={sortToggleDesc ? 
+                                        'program-icons-sort' : 'display-none' } id='program'>
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l7.5-7.5 7.5 7.5m-15 6l7.5-7.5 7.5 7.5" />
+                                        </svg>
+                                    </h3>
+                                </div>
+
+                            </button>
                         </div>
-                        <div className='program-card-grid'>
-                            <h3>Location</h3>
+                        <div className='program-card-grid'
+                            id='location'         
+                            value='location' 
+                        >
+                        <button 
+                            onClick={handleClick} 
+                            id='location' 
+                            value='location' 
+                            className='program-card-sort-btn'
+                        >
+                            <div className='program-card-sort'>
+                            <h3>Location
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" 
+                                    className={sortToggleAscLoc ? 
+                                    'program-icons-sort' : 'display-none'} id='location'>
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 5.25l-7.5 7.5-7.5-7.5m15 6l-7.5 7.5-7.5-7.5" />
+                                    </svg>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={sortToggleDescLoc ? 
+                                    'program-icons-sort' : 'display-none' } id='location'>
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l7.5-7.5 7.5 7.5m-15 6l7.5-7.5 7.5 7.5" />
+                                    </svg>
+
+                            </h3>
+                            </div>
+                        </button>
                         </div>
-                        <div className='program-card-grid'>
+                        <div className='program-card-grid  program-card-sort'>
                             <h3>Intake</h3>
                         </div>
-                        <div className='program-card-grid'>
-                            <h3>Program Status</h3>
+                        <div className='program-card-grid-none'>
+                        <button 
+                            onClick={handleClick} 
+                            id='program-status' 
+                            value='program-status' 
+                            className='program-card-sort-btn'
+                        >
+                            <div className='program-card-sort  program-card-sort'>
+                            <h3>Program Status
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" 
+                                        className={sortToggleAscProgStat ? 
+                                        'program-icons-sort' : 'display-none'} id='program-status'>
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 5.25l-7.5 7.5-7.5-7.5m15 6l-7.5 7.5-7.5-7.5" />
+                                        </svg>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={sortToggleDescProgStat ? 
+                                        'program-icons-sort' : 'display-none' } id='program-status'>
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l7.5-7.5 7.5 7.5m-15 6l7.5-7.5 7.5 7.5" />
+                                        </svg>                                
+                            </h3>
+                            </div>
+                            </button>
                         </div>
-                        <div className='program-card-grid'>
-                            <h3>Application Status</h3>
+                        <div className='program-card-grid-none'>
+                        <button 
+                            onClick={handleClick} 
+                            id='application-status' 
+                            value='application-status' 
+                            className='program-card-sort-btn'
+                        >
+                            <div className='program-card-sort'>
+                            <h3>Application Status
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" 
+                                        className={sortToggleAscAppStatus ? 
+                                        'program-icons-sort' : 'display-none'} id='application-status'>
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 5.25l-7.5 7.5-7.5-7.5m15 6l-7.5 7.5-7.5-7.5" />
+                                        </svg>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={sortToggleDescAppStatus ? 
+                                        'program-icons-sort' : 'display-none' } id='application-status'>
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l7.5-7.5 7.5 7.5m-15 6l7.5-7.5 7.5 7.5" />
+                                        </svg>                                
+                            </h3>
+                            </div>
+                            </button>
                         </div>
-                        <div className='program-card-grid'>
+                        <div className='program-card-grid-none'>
                             
                         </div>
                         </div>
@@ -95,7 +266,7 @@ function Programs() {
                                 <ProgramCard
                                     key={key}
                                     programData={programData}
-                                    onClick={deleteSingleProgram}
+                                    // onClick={deleteSingleProgram}
                                 />
                             
                         )
