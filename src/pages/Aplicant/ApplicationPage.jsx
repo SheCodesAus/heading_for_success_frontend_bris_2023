5b220765553afad3e3a67b3059cdb66b085ced3c
Mienke Dreyer <mienke.hattingh@gmail.com>
2023-10-23 12:47:31 +1000

    updated basic css to ApplicationForm+ ApplicPAge

--- src/pages/Aplicant/ApplicationPage.jsx
+++ src/pages/Aplicant/ApplicationPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ApplicationForm from '../../components/ApplicationForm/ApplicationForm';
+import './ApplicationPage.css'; 
+
 
 function Apply() {
     return (
         <div className="application-page">
             <div className="application-header">
-                <h1>Apply to Program</h1>
-                <p>Welcome to our application page. Please fill out the form below to apply.</p>
+            <h1 className="header">Apply to Program</h1>
+                <p className="subheader">Welcome to our application page. Please fill out the form below to apply.</p>
             </div>
             <div className="application-form-container">
                 <ApplicationForm />
