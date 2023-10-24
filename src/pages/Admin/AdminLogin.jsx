8ad9901dda2bdec08f6dc53f1a131af3123f6689
SKrpio <134825964+SKrpio@users.noreply.github.com>
2023-10-24 14:12:36 +1000

    Mienke's adminhome

--- src/pages/Admin/AdminLogin.jsx
+++ src/pages/Admin/AdminLogin.jsx
@@ -3,7 +3,9 @@ import LoginForm from "../../components/AdminLogin/LoginForm";
 function AdminLogin() {
     return(
         <>
-            <h1>This is the project page.</h1>
+            <br></br>
+            <h1>Welcome Administrator.</h1>
+            <br></br>
             <LoginForm />
         </>
     );
