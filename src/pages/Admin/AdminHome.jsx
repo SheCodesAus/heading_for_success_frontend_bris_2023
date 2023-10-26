7386f436015e6d2f650c845f45128963b0bb21f4
mmtovarm <monicamtovar@yahoo.com>
2023-10-26 20:58:03 +1000

    Admin Home Image sent to background

--- src/pages/Admin/AdminHome.jsx
+++ src/pages/Admin/AdminHome.jsx
@@ -28,7 +28,7 @@ function AdminHome() {
             </div>
             <Headeradmin />
             <div className="adminhome">
-            <img src="src/Images/Humaans1.svg" alt="Your image alt text" />
+            <img src="src/Images/Humaans1.svg" alt="Your image alt text" id="backgroundimage"/>
             </div>
             <Footer />
         </div>
