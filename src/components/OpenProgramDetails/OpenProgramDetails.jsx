88246c5f4b55d38135295fba96770b10844cfd20
mmtovarm <monicamtovar@yahoo.com>
2023-10-27 21:37:06 +1000

    styling open programs details

--- src/components/OpenProgramDetails/OpenProgramDetails.jsx
+++ src/components/OpenProgramDetails/OpenProgramDetails.jsx
@@ -1,8 +1,7 @@
 import { useOpenProgram } from "../../hooks/use-program";
 import { useState, Fragment } from 'react';
-
+import "./OpenProgramDetails.css";
 import { useParams, Link, useNavigate } from 'react-router-dom';
-
 
 function OpenProgramDetails() {
 
@@ -29,23 +28,20 @@ function OpenProgramDetails() {
 
     
     return (
-            <>
+            <div id="openprogramsdetail-body">
+            <img src={openProgramDetail.image} />
             <h1>Check out {openProgramDetail.program_name}!</h1>
 
             <h3>Location: {openProgramDetail.location}</h3>
-            <img src={openProgramDetail.image} />
+            
             <h3>Intake: {openProgramDetail.intake}</h3>
-
             <h3>Description: {openProgramDetail.description}</h3>
             <h3>Status: {openProgramDetail.status}</h3>
             <h3>Date Start: {openProgramDetail.date_start}</h3>
             <h3>Application Date Start: {openProgramDetail.application_date_start}</h3>
             <h3>Application Date End: {openProgramDetail.application_date_end}</h3>
-            <p></p>
-
             <button type="apply" onClick={()=>{toApplication()}}> Apply</button>
-
-            </>
+            </div>
     );
     
 }
